5c9264f4ffcac6fb993ce4f9bea9f37e2cb80447
Joel Santiago <jsant288@fiu.edu>
2021-11-01 04:23:13 +0000

    add box-shadow also made setTimeout to loop

--- src/js/component/Traffic.js
+++ src/js/component/Traffic.js
@@ -1,16 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 
 //create your first component
+
 const Traffic = () => {
-	const [toggle, setToggle] = useState("off");
+	const [light, setLight] = useState("red");
+
+	useEffect(() => {
+		const timer = setTimeout(function() {
+			if (light === "red") {
+				setLight("green");
+			} else if (light === "yellow") {
+				setLight("red");
+			} else if (light === "green") {
+				setLight("yellow");
+			} else useEffect(light);
+		}, 5000);
+	});
+
 	return (
 		<div className="lamp text-center mt-5 ">
 			<div className="col-2 mx-auto p-2 bg-dark rounded-pill ">
-				<div className="lights mx-auto my-2 rounded-circle bg-danger"></div>
-				<div className="lights mx-auto my-2 rounded-circle bg-warning"></div>
-				<div className="lights mx-auto my-2 rounded-circle bg-success"></div>
+				<div className="p-2">
+					{light === "red" ? (
+						<div className="lights redShadow mx-auto my-2 rounded-circle bg-danger"></div>
+					) : (
+						<div className="lights blackShadow mx-auto my-2 rounded-circle bg-dark"></div>
+					)}
+				</div>
+				<div className="p-2">
+					{light === "yellow" ? (
+						<div className="lights yellowShadow mx-auto my-2 rounded-circle bg-warning"></div>
+					) : (
+						<div className="lights blackShadow mx-auto my-2 rounded-circle bg-dark"></div>
+					)}
+				</div>
+				<div className="p-2">
+					{light === "green" ? (
+						<div className="lights greenShadow mx-auto my-2 rounded-circle bg-success"></div>
+					) : (
+						<div className="lights blackShadow mx-auto my-2 rounded-circle bg-dark"></div>
+					)}
+				</div>
 			</div>
 		</div>
 	);
